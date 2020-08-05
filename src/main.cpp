703a7954c521254fb6fdb933c93a8cef443a77dc
Connor Horman <chorman64@gmail.com>
2020-08-05 18:56:05 -0400

    Start option parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
+#include <string_view>
+#include <vector>
 
 #define VERSION "0.1" // TODO: This should be set in configuration
 
 int main(int argc, char *argv[])
 {
-    std::cout << "laserc bootstrap version " << VERSION << std::endl << std::endl;
-    std::cout << "Executable: " << argv[0] << std::endl;
-    std::cout << "Parameters: ";
-    for (int i = 1; i < argc; i++)
-    {
-        std::cout << argv[i];
-        if (i < argc - 1)
-            std::cout << ", ";
-    }
-    std::cout << std::endl;
+    std::vector<std::string_view> opts(argv,argv+argc);
 }
