ef8e7bb71e5d779540276534ccd6e6fd14c84b07
Ray Redondo <rdrpenguin04@gmail.com>
2020-12-14 08:32:48 -0600

    Deal with destructors, create debugging functions

--- src/ast.hpp
+++ src/ast.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <optional>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace laserc {
@@ -16,17 +17,26 @@ class ast_node {
   public:
     [[nodiscard]] uint64_t get_line() const noexcept;
     [[nodiscard]] uint32_t get_column() const noexcept;
-    [[nodiscard]] virtual const std::string &get_node_name() const noexcept = 0;
-    virtual ~ast_node();
+    [[nodiscard]] virtual std::string_view get_node_name() const noexcept = 0;
+    virtual ~ast_node() = 0;
 };
 
-class identifier_node : public ast_node {
+template<typename T> class value_node : public ast_node {
+  public:
+    [[nodiscard]] virtual const T &get_value() const noexcept = 0;
+    virtual ~value_node() = 0;
+};
+
+template<typename T> value_node<T>::~value_node() {}
+
+class identifier_node : public value_node<std::string> {
   private:
     std::string value;
 
   public:
-    identifier_node(std::string value) noexcept : value(std::move(value)) {}
-    [[nodiscard]] const std::string &get_value() const noexcept;
+    identifier_node(std::string value) noexcept;
+    [[nodiscard]] std::string_view get_node_name() const noexcept override;
+    [[nodiscard]] const std::string &get_value() const noexcept override;
 };
 
 class pattern_node : public ast_node {
@@ -34,8 +44,8 @@ class pattern_node : public ast_node {
     std::unique_ptr<identifier_node> name;
 
   public:
-    pattern_node(std::unique_ptr<identifier_node> name) noexcept
-        : name(std::move(name)) {}
+    pattern_node(std::unique_ptr<identifier_node> name) noexcept;
+    [[nodiscard]] std::string_view get_node_name() const noexcept override;
     [[nodiscard]] const identifier_node &get_name() const noexcept;
 };
 
@@ -44,8 +54,8 @@ class type_node : public ast_node {
     std::unique_ptr<identifier_node> name;
 
   public:
-    type_node(std::unique_ptr<identifier_node> name) noexcept
-        : name(std::move(name)) {}
+    type_node(std::unique_ptr<identifier_node> name) noexcept;
+    [[nodiscard]] std::string_view get_node_name() const noexcept override;
     [[nodiscard]] const identifier_node &get_name() const noexcept;
 };
 
@@ -56,8 +66,8 @@ class parameter_node : public ast_node {
 
   public:
     parameter_node(std::unique_ptr<pattern_node> pattern,
-                   std::unique_ptr<type_node> type) noexcept
-        : pattern(std::move(pattern)), type(std::move(type)) {}
+                   std::unique_ptr<type_node> type) noexcept;
+    [[nodiscard]] std::string_view get_node_name() const noexcept override;
     [[nodiscard]] const pattern_node &get_pattern() const noexcept;
     [[nodiscard]] const type_node &get_type() const noexcept;
 };
@@ -65,9 +75,13 @@ class parameter_node : public ast_node {
 class block_expression_node;
 
 class statement_node : public ast_node { // Base interface
+  public:
+    virtual ~statement_node() = 0;
 };
 
 class item_node : public statement_node { // Base interface
+  public:
+    virtual ~item_node() = 0;
 };
 
 class fn_node : public item_node {
@@ -79,9 +93,8 @@ class fn_node : public item_node {
   public:
     fn_node(std::unique_ptr<identifier_node> name,
             std::vector<std::unique_ptr<parameter_node>> parameters,
-            std::unique_ptr<block_expression_node> code) noexcept
-        : name(std::move(name)), parameters(std::move(parameters)),
-          code(std::move(code)) {}
+            std::unique_ptr<block_expression_node> code) noexcept;
+    [[nodiscard]] std::string_view get_node_name() const noexcept override;
     [[nodiscard]] const identifier_node &get_name() const noexcept;
     [[nodiscard]] const std::vector<std::unique_ptr<parameter_node>> &
     get_parameters() const noexcept;
@@ -89,6 +102,8 @@ class fn_node : public item_node {
 };
 
 class expression_without_block_node : public statement_node { // Base interface
+  public:
+    virtual ~expression_without_block_node() = 0;
 };
 
 class block_expression_node : public ast_node {
@@ -99,10 +114,9 @@ class block_expression_node : public ast_node {
   public:
     block_expression_node(
         std::vector<std::unique_ptr<statement_node>> statements,
-        std::unique_ptr<expression_without_block_node> ret_val) noexcept
-        : statements(std::move(statements)), ret_val(std::move(ret_val)) {}
-    [[nodiscard]] const std::vector<std::unique_ptr<statement_node>> &
-    get_statements() const noexcept;
+        std::unique_ptr<expression_without_block_node> ret_val) noexcept;
+    [[nodiscard]] std::string_view get_node_name() const noexcept override;
+    [[nodiscard]] const std::vector<std::unique_ptr<statement_node>> &get_statements() const noexcept;
     [[nodiscard]] expression_without_block_node *get_ret_val() const noexcept;
 };
 
