84f534ad992a9c2eb917e96f89bd0db9ffeb627f
Ray Redondo <rdrpenguin04@gmail.com>
2021-03-16 17:47:27 -0500

    Still broken. @InfernoDeity

--- src/program.cpp
+++ src/program.cpp
@@ -4,8 +4,8 @@ namespace cannon {
 
 program incomplete_program::to_program() const {
     std::vector<std::unique_ptr<function>> functions;
-    for(const incomplete_function *fn : m_functions) {
-        functions.push_back(std::make_unique<function>(fn->to_function()));
+    for(const incomplete_function *incomp_fn : m_functions) {
+        functions.push_back(std::unique_ptr<function>(&std::move(incomp_fn->to_function())));
     }
     return program(functions);
 }
