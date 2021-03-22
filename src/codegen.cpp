a5de6ac23214e59d54a95fc2d956673bb006291c
Ray Redondo <rdrpenguin04@gmail.com>
2021-03-21 21:26:01 -0500

    We generate a file! And it works!

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -3,7 +3,11 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/FileSystem.h>
 #include <llvm/Support/Host.h>
 #include <llvm/Support/TargetRegistry.h>
 #include <llvm/Support/TargetSelect.h>
@@ -11,11 +15,16 @@
 
 namespace cannon {
 
+llvm::Value* codegen_function_body(const std::vector<std::unique_ptr<statement>> &statements) {
+    return nullptr;
+}
+
 void codegen(program p) {
     llvm::LLVMContext context;
     llvm::Module module("Cannon Bootstrap Compiler", context);
 
     llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
 
     std::string targetTriple = llvm::sys::getDefaultTargetTriple();
     module.setTargetTriple(targetTriple);
@@ -31,6 +40,33 @@ void codegen(program p) {
     llvm::TargetMachine *targetMachine = target->createTargetMachine(targetTriple, "generic", "", options, rm);
 
     module.setDataLayout(targetMachine->createDataLayout());
+    module.setTargetTriple(targetTriple);
+
+    std::error_code errorCode;
+    llvm::raw_fd_ostream dest("a.o", errorCode);
+    if(errorCode) {
+        std::cerr << "Failed to open output file: " << errorCode.message() << std::endl;
+        abort();
+    }
+
+    llvm::legacy::PassManager pass;
+    if(targetMachine->addPassesToEmitFile(pass, dest, nullptr, llvm::CGFT_ObjectFile)) {
+        std::cerr << "Can't emit object file!" << std::endl;
+        abort();
+    }
+
+    llvm::IRBuilder<> builder(context);
+    for(const std::unique_ptr<function> &f_p : p.functions()) {
+        // Assume all functions return i32 and take no parameters
+        llvm::Function *function = llvm::Function::Create(llvm::FunctionType::get(llvm::Type::getInt32Ty(context), false), llvm::Function::ExternalLinkage, std::string(f_p->name()), module); // LLVM naming conventions are garbage
+        llvm::BasicBlock *block = llvm::BasicBlock::Create(context, "entry", function);
+        builder.SetInsertPoint(block);
+        builder.CreateRet(codegen_function_body(f_p->statements()));
+        llvm::verifyFunction(*function);
+    }
+
+    pass.run(module);
+    dest.flush();
 }
 
 }
