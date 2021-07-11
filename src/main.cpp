f70b18a90af2b49cf1ad896e622e96030905ba37
konsumlamm <konsumlamm@gmail.com>
2021-07-11 15:47:06 +0200

    Clean up
    Format a bit
    Update comments
    Use more auto

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ using namespace cannon;
 using namespace std::string_view_literals;
 
 int main(int argc, char *argv[]) {
+    // CLI
     std::vector<std::string_view> opts(argv, argv + argc);
     std::string_view output{"a.o"};
     std::vector<std::string_view> input_files{};
@@ -32,113 +33,107 @@ int main(int argc, char *argv[]) {
     std::string_view linker{CANNON_DEFAULT_LINKER};
     std::optional<std::string_view> sysroot{};
     std::string_view target{CANNON_DEFAULT_TRIPLE};
-    for (auto it = std::next(begin(opts)); it != end(opts);
-        it++) { // ADL too OP
-
-        std::string_view opt{*it};
-        if(opt=="--target"sv){
+    for (auto it = std::next(begin(opts)); it != end(opts); it++) { // ADL too OP
+        auto opt{*it};
+        if (opt == "--target"sv) {
             it++;
-            if(it == end(opts))
+            if (it == end(opts))
                 std::exit(1);
             target = *it;
-        }
-        else if(opt.starts_with("--target="sv))
+        } else if (opt.starts_with("--target="sv)) {
             target = opt.substr(9);
-        else if(opt=="--sysroot"){
+        } else if (opt=="--sysroot") {
             it++;
-            if(it == end(opts))
+            if (it == end(opts))
                 std::exit(1);
             sysroot = *it;
-        }
-        else if(opt.starts_with("--sysroot="sv))
+        } else if (opt.starts_with("--sysroot="sv)) {
             sysroot = opt.substr(10);
-        else if(opt.starts_with("-fuse-ld="sv))
+        } else if (opt.starts_with("-fuse-ld="sv)) {
             linker = opt.substr(10);
-        else if(opt == "--compile-only"sv)
+        } else if (opt == "--compile-only"sv) {
             mode = compiler_mode::CompileOnly;
-        else if(opt == "-ftype-check"sv || opt == "--check"sv)
+        } else if (opt == "-ftype-check"sv || opt == "--check"sv) {
             mode = compiler_mode::TypeCheck;
-        else if(opt == "-O"sv || opt == "-O2"sv)
+        } else if (opt == "-O"sv || opt == "-O2"sv) {
             opt_level = optimization_level{2};
-        else if(opt == "-O0"sv)
+        } else if (opt == "-O0"sv) {
             opt_level = optimization_level{};
-        else if(opt == "-O1"sv)
+        } else if (opt == "-O1"sv) {
             opt_level = optimization_level{1};
-        else if(opt == "-O3"sv)
+        } else if (opt == "-O3"sv) {
             opt_level = optimization_level{3};
-        else if(opt == "-Og"sv)
+        } else if (opt == "-Og"sv) {
             opt_level = optimization_level::Debug;
-        else if(opt == "-Os"sv)
+        } else if (opt == "-Os"sv) {
             opt_level = optimization_level::Size;
-        else if(opt == "-Oz"sv)
+        } else if (opt == "-Oz"sv) {
             opt_level = optimization_level::Zize;
-        else if(opt == "-Ofast"sv)
+        } else if (opt == "-Ofast"sv) {
             opt_level = optimization_level::Fast;
-        else if(opt == "-Oextra"sv)
+        } else if (opt == "-Oextra"sv) {
             opt_level = optimization_level::Extra;
-        else if(opt == "-c"sv)
+        } else if (opt == "-c"sv) {
             mode = compiler_mode::CompileOnly;
-        else if(opt == "-co"sv){
+        } else if (opt == "-co"sv) {
             mode = compiler_mode::CompileOnly;
             it++;
-            if(it == end(opts))
+            if (it == end(opts))
                 std::exit(1);
             output = *it;
-        }else if(opt.starts_with("-co"sv)){
+        } else if (opt.starts_with("-co"sv)) {
             mode = compiler_mode::CompileOnly;
             output = opt.substr(3);
-        }else if(opt=="-o"sv){
+        } else if (opt=="-o"sv) {
             it++;
-            if(it == end(opts))
+            if (it == end(opts))
                 std::exit(1);
             output = *it;
-        }else if(opt.starts_with("-o"sv))
-            output = opt.substr(2);  
-        else if(opt == "-L"sv){
+        } else if (opt.starts_with("-o"sv)) {
+            output = opt.substr(2);
+        } else if (opt == "-l"sv) {
             it++;
-            if(it == end(opts))
+            if (it == end(opts))
+                std::exit(1);
+            libs.push_back(*it);
+        } else if (opt.starts_with("-l"sv)) {
+            libs.push_back(opt.substr(2));
+        } else if (opt == "-L"sv) {
+            it++;
+            if (it == end(opts))
                 std::exit(1);
             libdirs.push_back(*it);
-        }else if(opt.starts_with("-L"sv))
+        } else if (opt.starts_with("-L"sv)) {
             libdirs.push_back(opt.substr(2));
-        else if(opt == "-L"sv){
-            it++;
-            if(it == end(opts))
-                std::exit(1);
-            libdirs.push_back(*it);
-        }else if(opt.starts_with("-L"sv))
-            libdirs.push_back(opt.substr(2));
-        else
+        } else {
             input_files.push_back(opt);
+        }
     }
 
-    static_cast<void>(input_files);
     static_cast<void>(output_type);
-    static_cast<void>(mode);
     static_cast<void>(linker);
     static_cast<void>(opt_level);
-    for(auto&& a : input_files){
+    for (auto&& a : input_files) {
         std::ifstream file{std::string{a}};
-        if(!file)
+        if (!file)
             std::exit(1);
-        std::vector<token> tokens = lex(file);
+        auto tokens = lex(file);
 
-        std::cout << "Tokens: {" << std::endl;
-        for (token t : tokens) {
-            std::cout << "\t{" << t.get_line() << " " << t.get_column() << " "
-                    << std::quoted(t.get_text()) << "}" << std::endl;
+        std::cout << "Tokens:" << std::endl;
+        for (auto token : tokens) {
+            std::cout << "\t{" << token.get_line() << ":" << token.get_column()
+                << " " << std::quoted(token.get_text()) << "}" << std::endl;
         }
-        std::cout << "}" << std::endl;
 
-        file_node parsed = parse_file(tokens);
+        auto parsed = parse_file(tokens);
 
         std::cout << "AST: " << parsed << std::endl;
 
-        program analysed = analyze(std::move(parsed));
+        auto analysed_program = analyze(std::move(parsed));
 
-        std::cout << "Analysed: " << analysed << std::endl;
-        if(mode<compiler_mode::TypeCheck)
-            codegen(std::move(analysed), std::string(output));
+        std::cout << "Analysed: " << analysed_program << std::endl;
+        if (mode < compiler_mode::TypeCheck)
+            codegen(std::move(analysed_program), std::string(output));
     }
 
     return 0;
--- src/mode.hpp
+++ src/mode.hpp
@@ -8,17 +8,15 @@
 namespace cannon {
 
 enum class compiler_mode {
-    CompileAndLink,  // Produce final binary, according to LinkType
-    GenerateObjects, // Produce object files for linking, may include
-                     // joined-module form.
-    CompileOnly,     // Compile and typecheck to .lsm (IR format)
-    TypeCheck,    // Check syntax and type validity, but does not generate any
-                  // artifacts
-    Dependencies, // Produces a dependency graph for the compilation, but does
-                  // not
-    ModuleServer, // Act as a module server, and marshall compilation of cannon
-                  // dependency graphs, by a combination of cannon runs using
-                  // -c/--compile-only, and --check.
+    CompileAndLink,  // produce final binary, according to the link type
+    GenerateObjects, // produce object files for linking, may include joined-module form
+    CompileOnly,     // compile and typecheck to IR format
+    TypeCheck,       // check syntax and type validity, but does not generate any artifacts
+    Dependencies,    // produce a dependency graph for the compilation, but does
+                     // not ...
+    ModuleServer,    // act as a module server, and marshall compilation of cannon
+                     // dependency graphs, by a combination of cannon runs using
+                     // -c/--compile-only, and --check
 };
 
 enum class link_type {
@@ -26,23 +24,22 @@ enum class link_type {
     JoinedModule, // .tbd
     SharedModule, // .so/.dll, with a tbd section
     StaticLib,    // .a/.lib, usable with C/not-cannon
-    SharedLib, // .so/.dll, usable with C/not-cannon, no cannon module information
-    Partial,   // .tbd or .tbd2, but incomplete
+    SharedLib,    // .so/.dll, usable with C/not-cannon, no cannon module information
+    Partial,      // .tbd or .tbd2, but incomplete
 };
 
 enum class optimization_level {
-    
     FirstReserved = 240,
 
     // -Og
     Debug = 250,
-    // -Ofast, not necessarily compliant
+    // -Ofast (not necessarily compliant)
     Fast = 252,
     // -Os
     Size = 253,
     // -Oz
     Zize = 254,
-    // -Oextra, not necessarily compliant
+    // -Oextra (not necessarily compliant)
     Extra = 255
 };
 
