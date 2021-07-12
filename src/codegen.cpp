fd6c98e70327475b3ba5c6b44b64f23e5fb06843
Ray Redondo <rdrpenguin04@gmail.com>
2021-07-11 23:28:42 -0500

    Hack together function call codegen

--- src/codegen.cpp
+++ src/codegen.cpp
@@ -2,6 +2,9 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <map>
+#include <utility>
+#include <vector>
 
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/IRBuilder.h>
@@ -24,10 +27,10 @@ std::string mangle(const function &fn) {
     return result;
 }
 
-llvm::Value* codegen_expr(const expression &expr, llvm::LLVMContext &context, llvm::IRBuilder<> &builder) {
+llvm::Value* codegen_expr(const expression &expr, llvm::LLVMContext &context, llvm::IRBuilder<> &builder, std::map<std::string, std::pair<llvm::Function*, const std::unique_ptr<function>*>> functions) {
     if(const binary_expression *bin_expr = dynamic_cast<const binary_expression*>(&expr)) {
-        llvm::Value *lhs = codegen_expr(bin_expr->lhs(), context, builder);
-        llvm::Value *rhs = codegen_expr(bin_expr->rhs(), context, builder);
+        llvm::Value *lhs = codegen_expr(bin_expr->lhs(), context, builder, functions);
+        llvm::Value *rhs = codegen_expr(bin_expr->rhs(), context, builder, functions);
         switch(bin_expr->op()) {
           case ADD:
             return builder.CreateAdd(lhs, rhs);
@@ -43,18 +46,21 @@ llvm::Value* codegen_expr(const expression &expr, llvm::LLVMContext &context, ll
     } else if(const integer_expression *int_expr = dynamic_cast<const integer_expression*>(&expr)) {
         return llvm::ConstantInt::get(context, llvm::APInt(32, int_expr->value() & 0x00000000FFFFFFFFULL, true));
     } else if(const function_call_expression *fn_expr = dynamic_cast<const function_call_expression*>(&expr)) {
-        static_cast<void>(fn_expr);
-        std::cerr << "WIP" << std::endl;
-        std::abort();
+        return builder.CreateCall(llvm::FunctionType::get(llvm::Type::getInt32Ty(context), false), codegen_expr(fn_expr->func(), context, builder, functions), std::vector<llvm::Value*>(), std::vector<llvm::OperandBundleDef>());
+    } else if(const identifier_expression *id_expr = dynamic_cast<const identifier_expression*>(&expr)) {
+        // Fun.
+        // So, for now, we're assuming identifiers refer to functions. This'll be dealt with in semantic analysis eventually.
+        // Also, semantic analysis will make it so we know which function is being referred to instead of having to assume signature as always
+        return functions[std::string("_C") + std::to_string(id_expr->value().size()) + id_expr->value() + "v"].first;
     } else {
         std::cerr << "Heh. Heh." << std::endl;
         return nullptr;
     }
 }
 
-llvm::Value* codegen_function_body(const std::vector<std::unique_ptr<statement>> &statements, llvm::LLVMContext &context, llvm::IRBuilder<> &builder) {
+llvm::Value* codegen_function_body(const std::vector<std::unique_ptr<statement>> &statements, llvm::LLVMContext &context, llvm::IRBuilder<> &builder, std::map<std::string, std::pair<llvm::Function*, const std::unique_ptr<function>*>> functions) {
     // FIXME: So, for now, I'm assuming there's only one expr. Because there is only one expr.
-    return codegen_expr(*(dynamic_cast<expression*>(&(*statements[0]))), context, builder); // Uh, this statement is garbage
+    return codegen_expr(*(dynamic_cast<expression*>(&(*statements[0]))), context, builder, functions); // Uh, this statement is garbage
 }
 
 void codegen(program p, std::string output_file) {
@@ -93,19 +99,30 @@ void codegen(program p, std::string output_file) {
         abort();
     }
 
-    llvm::IRBuilder<> builder(context);
+    std::map<std::string, std::pair<llvm::Function*, const std::unique_ptr<function>*>> functions;
+
     for(const std::unique_ptr<function> &f_p : p.functions()) {
-        llvm::Function *function;
+        std::string name;
+        llvm::FunctionType *type;
         if(f_p->name() == "main") {
             // All valid `main` functions have the same signature. All invalid `main` functions don't exist, NDR.
-            function = llvm::Function::Create(llvm::FunctionType::get(llvm::Type::getInt32Ty(context), false), llvm::Function::ExternalLinkage, std::string("main"), module);
+            type = llvm::FunctionType::get(llvm::Type::getInt32Ty(context), false);
+            name = std::string("main");
         } else {
             // FIXME: Currently assuming all functions return i32 and take no parameters.
-            function = llvm::Function::Create(llvm::FunctionType::get(llvm::Type::getInt32Ty(context), false), llvm::Function::ExternalLinkage, mangle(*f_p), module); // LLVM naming conventions are garbage
+            type = llvm::FunctionType::get(llvm::Type::getInt32Ty(context), false);
+            name = mangle(*f_p);
         }
+        functions[name] = std::pair<llvm::Function*, const std::unique_ptr<function>*>(llvm::Function::Create(type, llvm::Function::ExternalLinkage, name, module), &f_p);
+    }
+
+    llvm::IRBuilder<> builder(context);
+    for(auto const &[name, pair] : functions) {
+        auto function = pair.first;
+        auto &f_p = *pair.second;
         llvm::BasicBlock *block = llvm::BasicBlock::Create(context, "entry", function);
         builder.SetInsertPoint(block);
-        builder.CreateRet(codegen_function_body(f_p->statements(), context, builder));
+        builder.CreateRet(codegen_function_body(f_p->statements(), context, builder, functions));
         llvm::verifyFunction(*function);
     }
 
